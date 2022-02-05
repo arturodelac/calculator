aa1a5e22d27c1d8c6b70985d70a62bb9d9c1eb51
arturodelac <arturodelac1@gmail.com>
2022-02-04 19:25:59 -0600

    Basic Calculator functionality working, UI changes

--- javascript.js
+++ javascript.js
@@ -8,12 +8,12 @@ function add(...args) {
 }
 
 function substract(...args) {
-    const sumArray = Array.from(args);
-    console.log(sumArray);
-    const sum = sumArray.reduce((total, argument) => {
+    const substractArray = Array.from(args);
+    console.log(substractArray);
+    const substract = substractArray.reduce((total, argument) => {
         return total - argument;
-    }, (sumArray[0] * 2))
-    return sum;
+    }, (substractArray[0] * 2))
+    return substract;
 }
 
 function multiply(...args) {
@@ -27,4 +27,78 @@ function multiply(...args) {
 
 function divide(first, second) {
     return first / second;
+}
+
+const result = document.querySelector('.result');
+
+
+//buttons
+let value = '';
+let value2 = '';
+let operand = '';
+let operand2 = '';
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => button.addEventListener('click', () => {
+    if (isNaN(button.value) && button.value != '.') {
+        operand = button.value;
+        if (operand == '=') {
+            calculate();
+            result.textContent = value;
+            console.log(value)
+        } else {
+            operand2 = button.value;
+            console.log("operand2" + operand2);
+            result.textContent = operand2;
+        }
+    } else if (operand == '') {
+        value = value + button.value;
+        result.textContent = value;
+        console.log("value" + value)
+    } else if (operand != '' && operand != '=') {
+        value2 = value2 + button.value;
+        result.textContent = value2;;
+
+
+    }
+}))
+
+//evaluate 2 numbers at a time
+function calculate() {
+    value = parseFloat(value);
+    value2 = parseFloat(value2);
+
+
+    switch (operand2) {
+        case '+':
+            let sum = add(value, value2);
+            value = sum;
+            break;
+        case '-':
+            let substract = substract(value, value2);
+            value = substract;
+            break;
+        case '/':
+            if (value2 == 0) {
+                alert("You cannot divide by 0!");
+                value = '';
+                operand = '';
+                value2 = '';
+                break;
+            } else {
+                let division = divide(value, value2);
+                value = division;
+                break;
+            }
+
+
+
+
+
+
+    }
+
+
+
+
+
 }
